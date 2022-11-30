04967ad181e2fbd734972230cfe4d9ba0967f17a
Yordanka AGP <yordankaandre@gmail.com>
2022-11-30 15:34:45 +0700

    Added React Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import reportWebVitals from './reportWebVitals'
+import App from './App'
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom'
+
+const container = document.getElementById('root')!
+const root = createRoot(container)
+
+const router = createBrowserRouter(
+	createRoutesFromElements(<Route path='/' element={<App />}></Route>)
+)
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<Provider store={store}>
+			<CssBaseline />
+			<RouterProvider router={router} />
+		</Provider>
+	</React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
